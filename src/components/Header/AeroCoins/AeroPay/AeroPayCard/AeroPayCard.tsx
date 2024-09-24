8f698b2a7da8aefe0e29d748dbc444d6ac314096
Juan Manuel Ibarzabal Salles <63437981+ibarzabal-jm@users.noreply.github.com>
2024-09-24 15:20:41 -0300

    update card animation

--- src/components/Header/AeroCoins/AeroPay/AeroPayCard/AeroPayCard.tsx
+++ src/components/Header/AeroCoins/AeroPay/AeroPayCard/AeroPayCard.tsx
@@ -11,14 +11,16 @@ interface AeroPayCardProps {
 
 export const AeroPayCard = ({ user }: AeroPayCardProps) => {
   return (
-    <div className={styles.aeroCard}>
-      <div className={styles.aeroCardHeader}>
-        <p>Aerocard</p>
-        <AeroPayIcon className={styles.icon} />
-      </div>
-      <div className={styles.aeroCardFooter}>
-        <p className={styles.footerText}>{user.name}</p>
-        <p className={styles.footerText}>{formatDate(user.createDate)}</p>
+    <div className={styles.aeroCardContainer}>
+      <div className={styles.aeroCard}>
+        <div className={styles.aeroCardHeader}>
+          <p>Aerocard</p>
+          <AeroPayIcon className={styles.icon} />
+        </div>
+        <div className={styles.aeroCardFooter}>
+          <p className={styles.footerText}>{user.name}</p>
+          <p className={styles.footerText}>{formatDate(user.createDate)}</p>
+        </div>
       </div>
     </div>
   );
