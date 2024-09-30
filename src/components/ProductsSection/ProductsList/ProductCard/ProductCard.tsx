d9d56080f72e61d44675dc25715ab3d64c1beb1a
Juan Manuel Ibarzabal Salles <63437981+ibarzabal-jm@users.noreply.github.com>
2024-09-29 22:40:03 -0300

    feat: image

--- src/components/ProductsSection/ProductsList/ProductCard/ProductCard.tsx
+++ src/components/ProductsSection/ProductsList/ProductCard/ProductCard.tsx
@@ -1,30 +1,50 @@
-import Image from "next/image";
+import Image, { getImageProps } from "next/image";
 import styles from "./ProductCard.module.css";
 import { Product } from "@/services/types";
 import { BuyButton } from "./BuyButton/BuyButton";
+
+import { getImage } from "@/lib/getImage";
 
 interface ProductCardProps {
   product: Product;
   userPoints: number;
 }
 
-export const ProductCard: React.FC<ProductCardProps> = ({
+export const ProductCard: React.FC<ProductCardProps> = async ({
   product,
   userPoints,
 }) => {
+  const { base64 } = await getImage(product.img.hdUrl);
+
+  const common = {
+    alt: product.name,
+    width: 280,
+    height: 204,
+    blurDataURL: base64,
+  };
+  const {
+    props: { srcSet: desktop },
+  } = getImageProps({
+    ...common,
+    placeholder: "blur",
+    src: product.img.hdUrl,
+  });
+  const {
+    props: { srcSet: mobile, ...rest },
+  } = getImageProps({
+    ...common,
+    placeholder: "blur",
+    src: product.img.url,
+  });
+
   return (
     <div>
       <div className={styles.card}>
         <div className={styles.imageContainer}>
           <picture>
-            <source media="(min-width: 1240px)" srcSet={product.img.hdUrl} />
-            <Image
-              src={product.img.url}
-              alt={product.name}
-              width={280}
-              height={204}
-              sizes="(min-width: 1240px) 280px, 100vw"
-            />
+            <source media="(min-width: 1024px)" srcSet={desktop} />
+            <source media="(min-width: 500px)" srcSet={mobile} />
+            <img {...rest} />
           </picture>
         </div>
         <div className={styles.description}>
